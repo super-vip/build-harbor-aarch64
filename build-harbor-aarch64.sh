9ee6660cf696b972cd06e400570cea5dcd980d98
Mr. Yang <i-up@qq.com>
2025-04-11 22:14:49 +0800

    Update build-harbor-aarch64.sh

--- build-harbor-aarch64.sh
+++ build-harbor-aarch64.sh
@@ -13,7 +13,6 @@ cp -f ../harbor/Makefile src/github.com/goharbor/harbor/
 cp -f ../harbor/make/photon/Makefile src/github.com/goharbor/harbor/make/photon/
 cp -f ../harbor/make/photon/registry/builder src/github.com/goharbor/harbor/make/photon/registry/
 cp -f ../harbor/src/portal/src/app/shared/components/about-dialog/about-dialog.component.html src/github.com/goharbor/harbor/src/portal/src/app/shared/components/about-dialog/
-cp -r ../harbor/make/photon/core src/github.com/goharbor/harbor/make/photon/
 
 # compile redis
 make compile_redis
